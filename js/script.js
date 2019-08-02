5558c511bbfd04a07fa5441004eb8c9b3f35a3ee
Alexandr Mischenenko <alexxaandr.m@gmail.com>
2019-08-02 16:54:58 +0500

    new object 'shit'

--- js/script.js
+++ js/script.js
@@ -1,27 +1,9 @@
 var canvas = document.getElementById('game');
 var context = canvas.getContext('2d');
 
-var background = new Image();
-background.src = "./img/background.jpg";
+var y = 0;
 
-background.onload = function(){
-    game();
-    requestAnimFrame(game);
-}
-
-function game(){
-    
-    update();
-    render();
-}
-
-function update(){
-
-}
-
-function render(){
-    context.drawImage(background, 0,0);
-}
+//это нужно для бесконечного цикла, во всех браузерах
 var requestAnimFrame = (function(){
     return window.requestAnimationFrame ||
     window.webkitRequestAnimationFrame ||
@@ -32,3 +14,38 @@ var requestAnimFrame = (function(){
         window.setTimeout(callback, 1000 /20);
     };
 })();
+
+//переменная с фоном
+var background = new Image();
+background.src = "./img/background.jpg";
+
+//переменная с обьектом, который будет ловить главный герой
+var shit = new Image();
+shit.src = "./img/shit.png";
+
+//после того, как грузится картинка с фоном, запускается функция с игрой и зацикливается
+background.onload = function(){
+    game();
+   
+}
+
+//функция с игрой
+function game(){
+    requestAnimFrame(game);
+    update();
+    render();
+}
+
+function update(){
+    y=y+5;
+    //границы
+    if(y>=600){
+        y=0;
+    }
+}
+
+//функция подгрузки элементов
+function render(){
+    context.drawImage(background, 0,0);
+    context.drawImage(shit, 0, y, 50, 50);
+}
